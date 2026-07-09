fb981df340ffe90f7392ab3c91c78f6cab592875
Prince087 <149547853+Prince087@users.noreply.github.com>
2026-07-09 09:40:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (38.35%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,22 @@
+// Find the first decreasing element from the right (pivot).
+// Swap it with the next greater element on its right.
+// Reverse the remaining suffix (descending -> ascending)
+// to get the smallest permutation larger than the current one.
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i= n-2;
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = n-1;
+            while(j>=0 && nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums[j] , nums[i]);
+        }
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
